1f90d682f1b3f33117e28c69b54833d4a5a618ee
Ryan Evans <ryan@jungleberry.dev>
2024-06-19 12:08:32 -0500

    Added buttons to index.html to reset the grid and to set a grid size
    Two buttons have been added. One will eventually just reset the page to defaults, and the other will change the grid size. The reset to defaults does not function yet, but will in a future update.
    The grid size selector button does function, mostly. I need to add rules around the range that can be selected. I'm going with 5-75, somewhat arbitrarily to make sure I know how to do this.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   scripts.js
    	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -5,11 +5,35 @@ even possible. If not, I'll have to wrap it with flexbox maybe?
 
 2. I'll need to be able to set the grid size via a variable. This could determine the size of the grid. How do I set the number of divs via scripts and css?
 
+3. Assign a reset button
+
+4. Assign user input to change gridSize
+
+5. Create hover to change color of squares to default color
+
+6. Add user input of color
 */
 
 // Initialize variables
 const container = document.querySelector("#grid-container");
-let gridSize = 5;
+let gridSize = 10;
+
+createGrid(gridSize);
+
+// Set Grid Size Button Event Listener
+const setGridSize = document.querySelector("#set-grid-size");
+setGridSize.addEventListener("click", () => {
+  resetGrid();
+});
+
+// Function called when Set Grid Size is clicked. This will clear the container and rebuild based on user input
+function resetGrid() {
+  let gridSize = prompt("What grid size would you like? 5-75, please.");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  createGrid(gridSize);
+}
 
 // Create function to create the grid. Rows and cells will need to be created using the gridSize from the user
 function createGrid(gridSize) {
@@ -18,14 +42,13 @@ function createGrid(gridSize) {
     const row = document.createElement("div");
     row.classList.add("grid-row");
 
+    // Create cells and append to the row
     for (let i = 0; i < gridSize; i++) {
       const cell = document.createElement("div");
       cell.classList.add("grid-cell");
       row.appendChild(cell);
-      cell.textContent = "Cell!";
+      // cell.textContent = "Cell!";
     }
     container.appendChild(row);
   }
 }
-
-createGrid(gridSize);
